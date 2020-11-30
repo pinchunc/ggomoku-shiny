41c5c0c8f5efa75e17bfb0dc4ff8bd4e1649d49d
Pin-Chun Chen <pinchuc@uci.edu>
2020-11-30 14:02:30 -0800

    numeric input and plot output

--- template.R
+++ template.R
@@ -1,6 +1,19 @@
 library(shiny)
-ui <- fluidPage("Hello World")
+board_size <- 19
+ui <- fluidPage(
+  # *Input
+  numericInput(inputId = "num_x", label = "Choose a number for x coordinate",
+               value = 1, min = 1, max = board_size),
+  numericInput(inputId = "num_y", label = "Choose a number for x coordinate",
+               value = 1, min = 1, max = board_size),
+  # *Output
+  plotOutput("ggplot"))
 
-server <- function(input, output) {}
+server <- function(input, output) {
+  output$ggplot <- renderPlot({
+    title <- "100 random normal value"
+    plot(c(input$num_x, input$num_y), main = title)
+    })
+}
 
 shinyApp(ui = ui, server = server)
