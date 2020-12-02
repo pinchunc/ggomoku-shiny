813c9b5e148ae581bd1c08b9b697b9177c4e8ad4
Pin-Chun Chen <pinchuc@uci.edu>
2020-12-01 23:49:06 -0800

    sidebarPanel

--- template.R
+++ template.R
@@ -1,13 +1,23 @@
 library(shiny)
 board_size <- 19
+
+shinyApp(ui = ui, server = server)
 ui <- fluidPage(
-  # *Input
-  numericInput(inputId = "num_x", label = "Choose a number for x coordinate",
-               value = 1, min = 1, max = board_size),
-  numericInput(inputId = "num_y", label = "Choose a number for x coordinate",
-               value = 1, min = 1, max = board_size),
-  # *Output
-  plotOutput("ggplot"))
+  sidebarLayout(
+    sidebarPanel(
+      # *Input
+      numericInput(inputId = "num_x", label = "Choose a number for x coordinate",
+                   value = 1, min = 1, max = board_size),
+      numericInput(inputId = "num_y", label = "Choose a number for x coordinate",
+                   value = 1, min = 1, max = board_size)
+      ),
+    
+    mainPanel(
+      # *Output
+      plotOutput("ggplot")
+    )
+  )
+)
 
 server <- function(input, output) {
   output$ggplot <- renderPlot({
