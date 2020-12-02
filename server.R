e76b943b5273a29bce37e289f87c73885a609fe7
Pin-Chun Chen <pinchuc@uci.edu>
2020-12-02 15:44:01 -0800

    fix merge error

--- server.R
+++ server.R
@@ -1,9 +1,4 @@
 server <- function(input, output) {
-<<<<<<< HEAD
-  require(ggomoku)
-  # output$ggplot <- renderPlot({
-  #   gomoku_board()
-  # })
   #print out the iteration
   observeEvent(input$clicks, {
     print(as.numeric(input$clicks))
@@ -13,18 +8,8 @@ server <- function(input, output) {
   # Adds piece to the plotted grid
   add_tile <- reactive({annotate("point", x = input$num_x, y = input$num_y, size = 6.5, colour = color)
   })
-=======
->>>>>>> 793f7a6445e2416c5e357377d86367a36cff5000
+
   output$ggplot <- renderPlot({
     board + add_tile
   })
-  
-<<<<<<< HEAD
 }
-=======
-  #print out the iteration
-  observeEvent(input$clicks, {
-    print(as.numeric(input$clicks))
-  })
-}
->>>>>>> 793f7a6445e2416c5e357377d86367a36cff5000
