06bee9dd6de2db23e0fa5f98474ae18cc26cacbe
Pin-Chun Chen <pinchuc@uci.edu>
2020-12-02 14:45:47 -0800

    move functions from the package ggomoku

---
+++ game.R
@@ -0,0 +1,114 @@
+gomoku_board <- function(board_size = 19) {
+  
+  if (board_size == 15) {
+    board_size <- 15
+  }
+  else if (board_size == 19) {
+    board_size <- 19
+  }
+  else {
+    stop("board_size argument must be set to 19 (default) or 15.")
+  }
+  
+  
+  # Initializing data.frame for board
+  df <- data.frame(x = 1:board_size, y = 1:board_size)
+  
+  # Plotting the initial board
+  # Drawing board
+  board <- suppressMessages(
+    print(
+      ggplot(df) +
+        geom_point(aes(x, y), size = 5, alpha = 0) +
+        theme(
+          aspect.ratio = 1,
+          axis.ticks = element_line(colour = "black", size = 1),
+          panel.grid.major = element_line(colour = "black"),
+          panel.grid.minor = element_line(size = 1, colour = "black"),
+          panel.background = element_rect(fill = "#D2B48C"),
+          panel.ontop = FALSE
+        ) +
+        scale_x_continuous(
+          breaks = seq(1, nrow(df), by = 1),
+          minor_breaks = seq(1, nrow(df), 1)
+        ) +
+        scale_y_continuous(
+          breaks = seq(1, nrow(df), by = 1),
+          minor_breaks = seq(1, nrow(df), 1)
+        ) +
+        scale_x_continuous(
+          breaks = seq(1, nrow(df), by = 1),
+          minor_breaks = seq(1, nrow(df), 1),
+          sec.axis = dup_axis()
+        ) +
+        scale_y_continuous(
+          breaks = seq(1, nrow(df), by = 1),
+          minor_breaks = seq(1, nrow(df), 1),
+          sec.axis = dup_axis()
+        ) +
+        ggtitle("Black's Move", subtitle = "Move 1/60")
+    )
+  )
+  message("Both players have 60 tiles. Black is the first to move.")
+  
+  # Plotting the board
+  return(board)
+}
+
+gomoku_victory <- function(matrix) {
+  
+  # Pull out a column or a row from the matrix as a vector
+  winner <- NA
+  
+  # checking for winner on rows
+  if (is.na(winner)) {
+    rows_num <- split(matrix, row(matrix))
+    rows_rle <- lapply(rows_num, FUN = rle)
+    winner_list <- purrr::map(rows_rle, gomoku_winner)
+    if (any(!is.na(winner_list))) {
+      winner <- winner_list[!is.na(winner_list)]
+    }
+  }
+  
+  # checking for winner on columns
+  if (is.na(winner)) {
+    cols_num <- split(matrix, col(matrix))
+    cols_rle <- lapply(cols_num, FUN = rle)
+    winner_list <- purrr::map(cols_rle, gomoku_winner)
+    if (any(!is.na(winner_list))) {
+      winner <- winner_list[!is.na(winner_list)]
+    }
+  }
+  
+  # checking for winner on diagonals
+  # split the matrix to get diagonals
+  if (is.na(winner)) {
+    d <- row(matrix) - col(matrix)
+    diags_num <- split(matrix, d)
+    diags_rle <- lapply(diags_num, FUN = rle)
+    winner_list <- purrr::map(diags_rle, gomoku_winner)
+    if (any(!is.na(winner_list))) {
+      winner <- winner_list[!is.na(winner_list)]
+    }
+  }
+  
+  # split the matrix to get reverse diagonals
+  if (is.na(winner)) {
+    r <- row(matrix) + col(matrix)
+    rev_diags_num <- split(matrix, r)
+    rev_diags_rle <- lapply(rev_diags_num, FUN = rle)
+    winner_list <- purrr::map(rev_diags_rle, gomoku_winner)
+    if (any(!is.na(winner_list))) {
+      winner <- winner_list[!is.na(winner_list)]
+    }
+  }
+  return(winner)
+}
+
+gomoku_winner <- function(rle_output) {
+  if (5 %in% rle_output$lengths) {
+    winner_list <- rle_output$values[which(rle_output$lengths == 5)]}
+  else {winner_list <- NA}
+  return(winner_list)
+}
+
