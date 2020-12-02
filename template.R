b694e706b871d60a7f5838c7e584acea78933fa5
J Steven Raquel <jsteven.raquel8@gmail.com>
2020-12-02 13:59:21 -0800

    Adding calls to ggomoku, rsconnect libraries
    Changing the server call to call gomoku_board()

--- template.R
+++ template.R
@@ -1,4 +1,7 @@
 library(shiny)
+library(ggomoku)
+library(rsconnect)
+
 board_size <- 19
 
 shinyApp(ui = ui, server = server)
@@ -8,7 +11,7 @@ ui <- fluidPage(
       # *Input
       numericInput(inputId = "num_x", label = "Choose a number for x coordinate",
                    value = 1, min = 1, max = board_size),
-      numericInput(inputId = "num_y", label = "Choose a number for x coordinate",
+      numericInput(inputId = "num_y", label = "Choose a number for y coordinate",
                    value = 1, min = 1, max = board_size)
       ),
     
@@ -19,11 +22,18 @@ ui <- fluidPage(
   )
 )
 
+#server <- function(input, output) {
+#  output$ggplot <- renderPlot({
+#    title <- "100 random normal value"
+#    plot(c(input$num_x, input$num_y), main = title)
+#    })
+#}
+
 server <- function(input, output) {
+  require(ggomoku)
   output$ggplot <- renderPlot({
-    title <- "100 random normal value"
-    plot(c(input$num_x, input$num_y), main = title)
-    })
+    gomoku_board()
+  })
 }
 
 shinyApp(ui = ui, server = server)
