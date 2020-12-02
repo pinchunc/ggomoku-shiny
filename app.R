011df931966c64124e577c0209a6dde8f7b95b43
Pin-Chun Chen <pinchuc@uci.edu>
2020-12-02 14:46:16 -0800

    load the library

---
+++ app.R
@@ -0,0 +1,13 @@
+library(shiny)
+library(dplyr)
+library(tibble)
+library(purrr)
+library(ggplot2)
+library(ggiraph)
+
+# game.R needs to be loaded first
+source("R/game.R")
+source("R/ui.R")
+source("R/server.R")
+
+shinyApp(ui, server)
