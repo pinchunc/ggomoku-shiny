91346e206fe3dc23f57513f5d02374fa7f77b86d
J Steven Raquel <jsteven.raquel8@gmail.com>
2020-12-03 13:59:29 -0800

    Adding comments

--- R/server.R
+++ R/server.R
@@ -2,9 +2,11 @@ server <- function(input, output) {
 
   # Adds piece to the plotted grid
   output$game <- renderGirafe({
+    # depends on the actionButton with input_id = "submit"
     input$submit
     
-    dist <- isolate(add_tiles(input$x_coord,  input$y_coord,  board , "black"))
+    # Adding isolate() to avoid dependency
+    dist <- isolate(add_tiles(input$x_coord, input$y_coord, board, "black"))
     
 
   })
