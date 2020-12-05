1c3de224c1e91dfab5fc89956430ead97e8593c2
J Steven Raquel <jsteven.raquel8@gmail.com>
2020-12-05 06:17:01 -0800

    Testing reactives and observers.... This code doesn't work.

--- R/server.R
+++ R/server.R
@@ -3,16 +3,16 @@ server <- function(input, output) {
   # observeEvent(input$clicks, {
   #   print(as.numeric(input$clicks))
   # })
-  dataInput <- reactive({
-    testdata <- data.frame(
-      "x" = numeric(),
-      "y" = numeric()
-    )
-    testdata <- rbind(
-      testdata,
-      data.frame("x" = as.numeric(input$x_coord), "y" = as.numeric(input$y_coord))
-    )
-  })
+  #  dataInput <- reactive({
+  #    testdata <- data.frame(
+  #      "x" = numeric(),
+  #      "y" = numeric()
+  #
+  #   testdata <- rbind(
+  #      testdata,
+  #      data.frame("x" = as.numeric(input$x_coord), "y" = as.numeric(input$y_coord))
+  #    )
+  #  })
 
   # create input data frame
   # testdata <- data.frame("x" = "x",
@@ -25,59 +25,51 @@ server <- function(input, output) {
 
   # Adds piece to the plotted grid
   # board <- gomoku_board()
-  output$game <- renderGirafe({
+  # 
 
-    # input$submit is the number of times "submit" was clicked AKA move number
-    df_coord <- data.frame(move_num = numeric(),
-                           x = numeric(),
-                           y = numeric(),
-                           color = character()
-                           )
-    
-    isolate({
-      
-      if (input$submit %in% seq(1, 119, 2)) {
-        color <- "black"
-      }
-      else {
-        color <- "white"
-      }
-      # if the move number is odd, it's "black", else white
+  
 
+  
+  # when pressing the button, add a new row to the data.frame
 
-      # data frame of 120 potential moves
-      rbind(df_coord, c(input$submit, 
-                        input$x_coord, 
-                        input$y_coord, 
-                        color))
+  # reactive() is for calculating values, without side effects
+  # observe() is for performing actions, with side effects
 
-      df_coord
-    })
-
-    # to prevent the plot from appearing before the button has been clicked
-    # theoretically this is supposed to prevent the first move from being placed
-    # before the button has been clicked
-
-    if (input$submit == 0) {
-     return()
-    }
+  
+  globals <- reactiveValues(
+    data = data.frame(move_num = numeric(),
+                      move_color = character(),
+                      x = numeric(),
+                      y = numeric()
+                      )
+    )
+  
+  observeEvent(input$goButton, {
+  
+     if (input$goButton %in% seq(1, 119, 2)) {
+       color <- "black"
+     }
+     else {
+       color <- "white"
+     }
+     
+     globals$data <- globals$data %>% add_row(move_num = input$goButton,
+                                                  move_color = color,
+                                                  x = input$x_coord,
+                                                  y = input$y_coord)
+     
+     })
+  
+  output$plot <- renderGirafe({
 
     # What is dataInput()?
     # add_tiles(dataInput(), board, "black")
 
+    message("Adding new tiles...")
     add_tiles(
-      xy_coord = df_coord,
-      board = board)
+        xy_coord = data,
+        board = board
+      )
   })
-  # Adds piece to the plotted grid
 
-  # output$game <- renderGirafe({
-  #   # depends on the actionButton with input_id = "submit"
-  #   input$submit
-  #
-  #   # Adding isolate() to avoid dependency
-  #   dist <- isolate(add_tiles(input$x_coord, input$y_coord, board, "black"))
-  #
-  #
-  # })
 }
