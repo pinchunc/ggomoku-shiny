2e58213a9c31c8ce38dd087af11ef464eb2db1cf
Pin-Chun Chen <pinchuc@uci.edu>
2020-12-05 21:43:14 -0800

    store the x y coordinate in a dataframe and plot them on the board. also create empty board at the beginning

--- R/server.R
+++ R/server.R
@@ -1,39 +1,57 @@
+
+
 server <- function(input, output) {
   
-  observeEvent(input$goButton, {
+  # observeEvent(input$goButton, {
+  #   message("Printing button index...")
+  #   print(as.numeric(input$goButton))
+  #   
+  # })
+  
+  values <- reactiveValues()
+  values$df <- data.frame(x = numeric(),
+                          y = numeric())
+  newEntry <- observeEvent(input$goButton, {
     message("Printing button index...")
     print(as.numeric(input$goButton))
-    
+    values$df <- rbind(values$df,
+                       data.frame(x = as.numeric(input$x_coord), 
+                                  y = as.numeric(input$y_coord)))
+  })
+  
+  dataInput <- reactive({
+    values$df
   })
   
   message("Initializing reactive data.frame...")
-  dataInput <- reactive({ 
-    rbind(data.frame(x = numeric(), 
-                     y = numeric()),
-          data.frame(x = as.numeric(input$x_coord), 
-                     y = as.numeric(input$y_coord))
-          )
-  })  
-
-
-  message("Calling renderGirafe...")
+  # dataInput <- eventReactive(input$goButton, {
+  #     isolate(df[nrow(df) + 1,] <- c(input$x_coord, input$y_coord))
+  # })
+  #output$table <- renderTable({df})
+  #Initial Dataframe
+  # df <- data.frame(x = numeric(),
+  #                  y = numeric())
+  # dataInput <- eventReactive(input$goButton,{ 
+  #   df <- rbind(df, data.frame(x = as.numeric(input$x_coord), 
+  #                              y = as.numeric(input$y_coord)))
+  #   return(df)
+  # })
   
+  message("Calling renderGirafe...")
   output$plot <- renderGirafe({
     
     message("Updating the data.frame")
-    #df <- data.frame(x = numeric(),
-    #                 y = numeric())
-    newdata <- dataInput()
-    df <- rbind(df, newdata)
-    
-    print(head(df))
+    # newdata <- dataInput()
+    # print(newdata)
+    # df <- rbind(df, newdata)
+    print(dataInput())
     # If the button hasn't been pressed yet, initialize a new board
     if (input$goButton == 0) {
       board <- gomoku_board()
     }
     
     # Plot new board (returns girafe object)
-    plot_new_board(df, board, "black")
+    plot_new_board(dataInput(), board, "black")
   })
 
 }
