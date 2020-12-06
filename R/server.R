55cec554a637128ff2449e1e6e84f4585928a7ed
J Steven Raquel <jsteven.raquel8@gmail.com>
2020-12-05 16:09:07 -0800

    Reverting to a functional version

--- R/server.R
+++ R/server.R
@@ -1,75 +1,34 @@
 server <- function(input, output) {
-  # print out the iteration
-  # observeEvent(input$clicks, {
-  #   print(as.numeric(input$clicks))
-  # })
-  #  dataInput <- reactive({
-  #    testdata <- data.frame(
-  #      "x" = numeric(),
-  #      "y" = numeric()
-  #
-  #   testdata <- rbind(
-  #      testdata,
-  #      data.frame("x" = as.numeric(input$x_coord), "y" = as.numeric(input$y_coord))
-  #    )
-  #  })
-
-  # create input data frame
-  # testdata <- data.frame("x" = "x",
-  #                        "y" = "y")
-  # finaldata = eventReactive(input$action, {
-  #   testdata = rbind(testdata,
-  #                    data.frame("x" = input$x_coord, "y" = input$y_coord))
-  # })
-  # print({testdata})
-
-  # Adds piece to the plotted grid
-  # board <- gomoku_board()
-  # 
-
-  
-
-  
-  # when pressing the button, add a new row to the data.frame
-
-  # reactive() is for calculating values, without side effects
-  # observe() is for performing actions, with side effects
-
-  
-  globals <- reactiveValues(
-    data = data.frame(move_num = numeric(),
-                      move_color = character(),
-                      x = numeric(),
-                      y = numeric()
-                      )
-    )
   
   observeEvent(input$goButton, {
+    message("Printing button index...")
+    print(as.numeric(input$goButton))
+    
+  })
   
-     if (input$goButton %in% seq(1, 119, 2)) {
-       color <- "black"
-     }
-     else {
-       color <- "white"
-     }
-     
-     globals$data <- globals$data %>% add_row(move_num = input$goButton,
-                                                  move_color = color,
-                                                  x = input$x_coord,
-                                                  y = input$y_coord)
-     
-     })
-  
+  message("Initializing reactive data.frame...")
+  dataInput <- reactive({ 
+    rbind(data.frame(x = numeric(), 
+                     y = numeric()),
+          data.frame(x = as.numeric(input$x_coord), 
+                     y = as.numeric(input$y_coord))
+          )
+  })  
+
+  message("Calling renderGirafe...")
   output$plot <- renderGirafe({
-
-    # What is dataInput()?
-    # add_tiles(dataInput(), board, "black")
-
-    message("Adding new tiles...")
-    add_tiles(
-        xy_coord = data,
-        board = board
-      )
+    
+    message("Updating the data.frame")
+    df <- data.frame(x = numeric(),
+                     y = numeric())
+    newdata <- dataInput()
+    df <- rbind(df, newdata)
+    
+    if (input$goButton == 0) {
+      board <- gomoku_board()
+    }
+    
+    plot_new_board(df, board, "black")
   })
 
 }
